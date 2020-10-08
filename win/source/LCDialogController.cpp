019e891235c3c3be335438c6fffada33ae7ddd4a
ivan.bakurevich <ivan.bakurevich@intexsoft.by>
2020-10-08 12:28:25 +0300

    Code refactoring.

--- win/source/LCDialogController.cpp
+++ win/source/LCDialogController.cpp
@@ -34,20 +34,16 @@
 #include <ILayoutUIUtils.h>
 #include <IPageList.h>
 #include <IStoryList.h>
-#include <ITextModel.h>
-#include <IComposeScanner.h>
-#include <IAutoFlowCmdData.h>
-#include <IDataStringUtils.h>
-#include <ITextStoryThreadDict.h>
 #include <IWaxIterator.h>
 #include <IWaxStrand.h>
 #include <IWaxLine.h>
 #include <CAlert.h>
 #include <IDocument.h>
-#include <IDataBase.h>
 #include <ISpread.h>
 #include <ISpreadList.h>
 #include <IGraphicFrameData.h>
+
+#include "Utilities.h";
 
 /** LCDialogController
 	Methods allow for the initialization, validation, and application of dialog widget
@@ -59,6 +55,69 @@
 */
 class LCDialogController : public CDialogController
 {
+private:
+	std::vector<std::string> getPageItems(std::string initStr) {
+		std::vector<std::string> vector;
+
+		initStr.erase(std::remove(initStr.begin(), initStr.end(), ' '), initStr.end());
+
+		std::stringstream ss(initStr);
+		std::string itemStr;
+		while (getline(ss, itemStr, ',')) {
+			vector.push_back(itemStr);
+		}
+		return vector;
+	}
+
+	std::vector<int32> getPageNumbersFromPageItems(std::vector<std::string>& pageItems) {
+		std::vector<int32> pageNumbers;
+
+		for (auto it = pageItems.begin(); it != pageItems.end(); it++) {
+			std::string pageItem = *it;
+
+			if (isNumber(pageItem)) {
+				int32 pageIndex = std::stoi(pageItem);
+				pageNumbers.push_back(pageIndex);
+			}
+			else if (isInterval(pageItem)) {
+				int32 dashIndex = pageItem.find('-');
+				int32 leftBorder = std::stoi(pageItem.substr(0, dashIndex));
+				int32 rightBorder = std::stoi(pageItem.substr(dashIndex + 1, pageItem.size()));
+
+				for (int i = leftBorder; i <= rightBorder; i++) {
+					pageNumbers.push_back(i);
+				}
+			}
+		}
+		return pageNumbers;
+	}
+
+	WideString pageItemsValidationError(std::vector<std::string>& pageItems, int32 docTotalPages) {
+		for (auto it = pageItems.begin(); it != pageItems.end(); it++) {
+			std::string pageItem = *it;
+
+			if (isNumber(pageItem)) {
+				int32 pageIndex = std::stoi(pageItem);
+				if (!isPageValid(pageIndex, docTotalPages)) {
+					return (WideString)"Validation Error. Page index out of range.";
+				}
+			}
+			else if (isInterval(pageItem)) {
+				int32 dashIndex = pageItem.find('-');
+				int32 leftBorder = std::stoi(pageItem.substr(0, dashIndex));
+				int32 rightBorder = std::stoi(pageItem.substr(dashIndex + 1, pageItem.size()));
+
+				if (!isPageIntervalValid(leftBorder, rightBorder, docTotalPages)) {
+					return (WideString)"Validation Error. Interval error.";
+				}
+			}
+			else {
+				return (WideString)"Validation Error. Only integers and symbols - , allowed.";
+			}
+		}
+		return WideString();
+	}
+
 	public:
 		/** Constructor.
 			@param boss interface ptr from boss object on which this interface is aggregated.
@@ -92,6 +151,7 @@ class LCDialogController : public CDialogController
 
 CREATE_PMINTERFACE(LCDialogController, kLCDialogControllerImpl)
 
+
 /* ApplyFields
 */
 void LCDialogController::InitializeDialogFields(IActiveContext* dlgContext)
@@ -105,182 +165,69 @@ void LCDialogController::InitializeDialogFields(IActiveContext* dlgContext)
 WidgetID LCDialogController::ValidateDialogFields(IActiveContext* myContext)
 {
 	WidgetID result = CDialogController::ValidateDialogFields(myContext);
+	std::vector<std::string> pageItems;
+	WideString errorMessage;
 
-	PMString pagesInputBoxString = this->GetTextControlData(kLCTextEditBoxWidgetID);
+	PMString inputBoxPMString = this->GetTextControlData(kLCTextEditBoxWidgetID);
 	//	empty string don't need validation.
-	if (pagesInputBoxString.IsEmpty()) {
+	if (inputBoxPMString.IsEmpty()) {
 		return result;
 	}
+
+	pageItems = getPageItems(inputBoxPMString.GetUTF8String());
 
 	IDocument* currentDocument = myContext->GetContextDocument();
 	//	can't apply validation without active document.
 	if (currentDocument == nil) {
 		return result;
 	}
+	InterfacePtr<IPageList> pageList(currentDocument, UseDefaultIID());
 
-	InterfacePtr<IPageList> pageList((IPMUnknown*)currentDocument, IID_IPAGELIST);
-	int32 docPagesCount = pageList->GetPageCount();
+	errorMessage = pageItemsValidationError(pageItems, pageList->GetPageCount());
 
-
-
-	std::string initStr = pagesInputBoxString.GetUTF8String();
-	std::vector<std::string> vectorStr;
-	std::vector<int32> vectorInt;
-
-	initStr.erase(std::remove(initStr.begin(), initStr.end(), ' '), initStr.end());
-
-	std::stringstream ss(initStr);
-	std::string numStr;
-	while (getline(ss, numStr, ',')) {
-		vectorStr.push_back(numStr);
+	if (!errorMessage.empty()) {
+		result = kLCTextEditBoxWidgetID;
+		CAlert::ErrorAlert(errorMessage);
 	}
-
-
-
-
-	for (auto it = vectorStr.begin(); it != vectorStr.end(); it++) {
-		std::string currStr = *it;
-		bool isDigit = true;
-		bool isInterval = true;
-		for (int i = 0; i < currStr.size(); i++) {
-			if (currStr[i] < '0' || currStr[i] > '9') {
-				isDigit = false;
-				if (currStr[i] != '-') {
-					isInterval = false;
-				}
-			}
-		}
-
-		if (isDigit) {
-			int32 intValue = std::stoi(currStr);
-			if (intValue<0 || intValue> docPagesCount) {
-				result = kLCTextEditBoxWidgetID;
-				CAlert::InformationAlert("Value is out of range.");
-			}
-			vectorInt.push_back(intValue);
-		}
-		else if (isInterval) {
-			int32 leftBorder;
-			int32 rightBorder;
-			size_t found;
-			if ((found = currStr.find('-')) != std::string::npos) {
-				leftBorder = std::stoi(currStr.substr(0, found));
-				rightBorder = std::stoi(currStr.substr(found + 1, std::string::npos));
-			}
-
-			if (leftBorder >= rightBorder) {
-				result = kLCTextEditBoxWidgetID;
-				CAlert::InformationAlert("Invalid interval.");
-			}
-
-			if (leftBorder<0 || leftBorder > docPagesCount ||
-				rightBorder<0 || rightBorder > docPagesCount) {
-				result = kLCTextEditBoxWidgetID;
-				CAlert::InformationAlert("Value is out of range.");
-			}
-
-			for (int i = leftBorder; i <= rightBorder; i++) {
-				vectorInt.push_back(i);
-			}
-		}
-		else {
-			result = kLCTextEditBoxWidgetID;
-			CAlert::InformationAlert("Only integers and symbols - , allowed.");
-		}
-	}
-
 
 	return result;
 }
+
+
 
 /* ApplyFields
 */
 void LCDialogController::ApplyDialogFields(IActiveContext* myContext, const WidgetID& widgetId)
 {
-	int xcount = 0;
 	int32 finalLinesCount = 0;
 	int32 pageLinesCount = 0;
 	int32 itemLinesCount = 0;
+	bool8 isCountInFullDocument;
+	std::vector<std::string> pageItems;
+	//	page indices starts from 1.
+	std::vector<int32> pageNumbers;
+
 	WideString output;
 	WideString noActiveDocumentErrorMessage((WideString)"LC plugin: No active document found! Please, open existing one.");
 
 	IDocument* currentDocument = myContext->GetContextDocument();
 	if (currentDocument == nil) {
+		//	do nothing without active document.
 		CAlert::WarningAlert(noActiveDocumentErrorMessage);
 		return;
 	}
 
+	PMString inputBoxPMString = this->GetTextControlData(kLCTextEditBoxWidgetID);
 
-
-	bool8 isCountInFullDocument = kFalse;
-	PMString pagesInputBoxString = this->GetTextControlData(kLCTextEditBoxWidgetID);
-
-	//	page indices starts from 1.
-	std::vector<int32> pagesIndicesVector;
-
-	if (pagesInputBoxString.IsEmpty()) {
+	// count in full documnet if empty.
+	if (inputBoxPMString.IsEmpty()) {
 		isCountInFullDocument = kTrue;
 	}
 	else {
-
-
-		std::string initStr = pagesInputBoxString.GetUTF8String();
-		std::vector<std::string> vectorStr;
-
-
-
-		initStr.erase(std::remove(initStr.begin(), initStr.end(), ' '), initStr.end());
-
-		std::stringstream ss(initStr);
-		std::string numStr;
-		while (getline(ss, numStr, ',')) {
-			vectorStr.push_back(numStr);
-		}
-
-
-
-
-		for (auto it = vectorStr.begin(); it != vectorStr.end(); it++) {
-			std::string currStr = *it;
-			bool isDigit = true;
-			bool isInterval = true;
-			for (int i = 0; i < currStr.size(); i++) {
-				if (currStr[i] < '0' || currStr[i] > '9') {
-					isDigit = false;
-					if (currStr[i] != '-') {
-						isInterval = false;
-					}
-				}
-			}
-
-			if (isDigit) {
-				int32 intValue = std::stoi(currStr);
-				pagesIndicesVector.push_back(intValue);
-			}
-			else if (isInterval) {
-				int32 leftBorder;
-				int32 rightBorder;
-				size_t found;
-				if ((found = currStr.find('-')) != std::string::npos) {
-					leftBorder = std::stoi(currStr.substr(0, found));
-					rightBorder = std::stoi(currStr.substr(found + 1, std::string::npos));
-				}
-
-				for (int i = leftBorder; i <= rightBorder; i++) {
-					pagesIndicesVector.push_back(i);
-				}
-			}
-		}
-
-
+		isCountInFullDocument = kFalse;
+		pageItems = getPageItems(inputBoxPMString.GetUTF8String());
+		pageNumbers = getPageNumbersFromPageItems(pageItems);
 	}
-
-
-
-
-
-
-
 
 
 	IDataBase* db = currentDocument->GetDocWorkSpace().GetDataBase();
@@ -299,7 +246,8 @@ void LCDialogController::ApplyDialogFields(IActiveContext* myContext, const Widg
 			documentPageIndex++;
 
 			if (isCountInFullDocument == kFalse &&
-				std::find(pagesIndicesVector.begin(), pagesIndicesVector.end(),documentPageIndex) == pagesIndicesVector.end()) {
+				//	skip if no such value in vector
+				std::find(pageNumbers.begin(), pageNumbers.end(), documentPageIndex) == pageNumbers.end()) {
 				continue;
 			}
 
@@ -315,10 +263,10 @@ void LCDialogController::ApplyDialogFields(IActiveContext* myContext, const Widg
 
 				//InterfacePtr<ITextModel> textModel(pageItemRef, UseDefaultIID());
 				InterfacePtr<IGraphicFrameData> graphicFrameData(pageItemRef, UseDefaultIID());
-				if (!graphicFrameData) { break; }
+				if (!graphicFrameData) { continue; }
 
 				InterfacePtr<IMultiColumnTextFrame> multiColumnTextFrame(graphicFrameData->QueryMCTextFrame());
-				if (!multiColumnTextFrame) { break; }
+				if (!multiColumnTextFrame) { continue; }
 				InterfacePtr<ITextModel> textModel(multiColumnTextFrame->QueryTextModel());
 
 				if (textModel) {
